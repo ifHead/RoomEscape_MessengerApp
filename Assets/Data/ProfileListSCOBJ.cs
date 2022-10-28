14c0676dc66ff9d25d72391ebe5b96b477564662
Sanhead <ijn7uhb@gmail.com>
2022-10-28 12:20:58 +0900

    initial commit _ gitignore included

---
+++ Assets/Data/ProfileListSCOBJ.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ProfileListSCOBJ", menuName = "Data/ProfileList", order = 1)]
+public class ProfileListSCOBJ : ScriptableObject
+{
+    public int color;
+}
---
+++ Assets/ScriptableOBJ/Profile.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Profile")]
+public class Profile : ScriptableObject
+{
+    [TextArea(10, 100)]
+    public string textAreaString = "1. name, color, introduce의 개수는 같아야 합니다.\n2.Department 부서명은 구분선으로 쓰입니다. \n3.구분선이 나와야 하는 곳은 name을 공란으로 둡니다.";
+    public string[] department;
+    public string[] name;
+    public string[] color;
+    public string[] introduce;
+    public GameObject[] chatRoom;
+}
