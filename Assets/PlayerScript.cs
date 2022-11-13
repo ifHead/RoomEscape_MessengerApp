152f58a714aa8fbc91bbccab80adad50718fe0a2
Sanhead <ijn7uhb@gmail.com>
2022-11-14 07:50:27 +0900

    Server - Client Testing

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,41 @@
+using Mirror;
+using UnityEngine;
+
+namespace QuickStart
+{
+    public class PlayerScript : NetworkBehaviour
+    {
+        public string dpStr;
+
+        [SyncVar(hook = nameof(strChange))]
+        public string myStr;
+
+        public void strChange(string oldValue, string newValue){
+            dpStr = myStr;
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            string s = UnityEngine.Random.Range(0,101010).ToString();
+            cmdSetup(s);
+        }
+
+        [Command]
+        public void cmdSetup(string s)
+        {
+            myStr = s;
+        }
+
+
+        void Update()
+        {
+            if (!isLocalPlayer) { return; }
+
+            float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
+            float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
+
+            transform.Rotate(0, moveX, 0);
+            transform.Translate(0, 0, moveZ);
+        }
+    }
+}
---
+++ Assets/Script/Server.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine.UI;
+
+public class Server : MonoBehaviour
+{
+    public Text serverText;
+}
